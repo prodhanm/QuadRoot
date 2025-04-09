1fb049d913a03a1362e6d1d90d4727bb68621235
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-09 16:56:13 -0400

    The quadroot is working and the results are accurate.

--- QuadRoot.cpp
+++ QuadRoot.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+float quadRoot(float a, float b, float c) {
+	float rootPos, rootNeg;
+	float discriminant = pow(b,2) - (4 * a * c);
+	rootPos = (-b + sqrt(discriminant)) / (2 * a);
+	rootNeg = (-b - sqrt(discriminant)) / (2 * a);
+	return rootPos, rootNeg;
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Enter the coefficients a, b, c: ";
+	float a, b, c;
+	cin >> a >> b >> c;
+	float result = quadRoot(a, b, c);
+	cout << "The roots are: " << result << endl;
 }
 
 
